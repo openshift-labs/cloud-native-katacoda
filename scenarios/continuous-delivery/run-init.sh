3f06d678cd222b1896bccac5686bdca133a9b70c
Ben Hall <ben@benhall.me.uk>
2017-09-06 13:13:00 +0100

    Download projects for each scenario until sticky sessions organised

--- scenarios/continuous-delivery/run-init.sh
+++ scenarios/continuous-delivery/run-init.sh
@@ -1,2 +1,4 @@
+ssh root@host01 "yum install unzip -y; curl -sL -o projects.zip https://github.com/openshift-roadshow/cloud-native-labs/archive/master.zip && unzip -q projects.zip && mv cloud-native-labs-master/*/ . && rm -rf cloud-native-labs-master && rm -f projects.zip"
+
 # ssh root@host01 "oc new-project gogs"
 # ssh root@host01 "oc process -f http://bit.ly/openshift-gogs-persistent-template --param=HOSTNAME=gogs-gogs.$(minishift ip).nip.io --param=GOGS_VERSION=0.9.113 --param=SKIP_TLS_VERIFY=true | oc create -f -"
