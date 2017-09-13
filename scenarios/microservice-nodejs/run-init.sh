50b4af1b1c5db59cc2a843bbd00cb82048020c14
Ben Hall <ben@benhall.me.uk>
2017-09-14 00:08:41 +0100

    Fix index.json intro section. This stopped run scripts launching. Base image updated to include unzip and samples.

--- scenarios/microservice-nodejs/run-init.sh
+++
@@ -1,1 +0,0 @@
-ssh root@host01 "yum install unzip -y; curl -sL -o projects.zip https://github.com/openshift-roadshow/cloud-native-labs/archive/master.zip && unzip -q projects.zip && mv cloud-native-labs-master/*/ . && rm -rf cloud-native-labs-master && rm -f projects.zip"
--- scenarios/microservice-wildfly-swarm/run-init.sh
+++ scenarios/microservice-wildfly-swarm/run-init.sh
@@ -1,3 +1,1 @@
-ssh root@host01 "yum install unzip -y; curl -sL -o projects.zip https://github.com/openshift-roadshow/cloud-native-labs/archive/master.zip && unzip -q projects.zip && mv cloud-native-labs-master/*/ . && rm -rf cloud-native-labs-master && rm -f projects.zip"
-
 ssh root@host01 "oc new-project coolstore"
